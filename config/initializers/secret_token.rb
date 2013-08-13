a51893bbe56518afafacf45554e21adfe7b01fea
Kyrie <kyrie.ak@gmail.com>
2013-08-13 15:32:53 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ono::Application.config.secret_key_base = 'fa44b24512896d2ad08ef865b6c390b621ebb38c343cc014c2bcaeebd91cc26a69a558169de0c871d507a4146f56cf39f812d51d7036ba308e666e830ed13c9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ono::Application.config.session_store :cookie_store, key: '_Ono_session'
