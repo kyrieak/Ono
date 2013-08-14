25bd1c5ec8df725ae672a36929403d188f7a325b
Kyrie <kyrie.ak@gmail.com>
2013-08-13 19:18:38 -0700

    added bootstrap custom assets and started the main view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Ono::Application.routes.draw do
+  get "links/new"
+  get "links/create"
+  get "links/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+  root 'links#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
